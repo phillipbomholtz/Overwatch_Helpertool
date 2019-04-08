8bb8d6f8bc5720418e9e7499e1f336529cbebc42
phillipbomholtz <phillip.bomholtz@gmail.com>
2019-04-08 15:13:04 +0200

    First commet for overwatch helper program. added a main menue. some jpegs and two panels

---
+++ src/overwatchhelpertool/OverwatchHelperTool.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package overwatchhelpertool;
+import javax.swing.*;
+/**
+ *
+ * @author phill
+ */
+public class OverwatchHelperTool {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JFrame vindue = new JFrame( "Choose a map" ); 
+        vindue.add( new Menu() );
+        vindue.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); 
+        vindue.pack();
+        vindue.setVisible(true);
+    }
+    
+}

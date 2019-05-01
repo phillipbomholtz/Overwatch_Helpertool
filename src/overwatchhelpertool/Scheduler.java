d90bd06ddc989dcb79d0e5ae4ab39dcc7476cb8b
phillipbomholtz <phillip.bomholtz@gmail.com>
2019-05-01 09:07:11 +0200

    made the defining class (struck) of what a timestrech is and used in program. deleted junk files

--- src/overwatchhelpertool/Scheduler.java
+++ src/overwatchhelpertool/Scheduler.java
@@ -1,32 +1,36 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * This class is for calculating the avilabilety of team
  */
 package overwatchhelpertool;
 
 /**
  *
- * @author phill
+ * @author Phillip Bomholtz
  */
 public class Scheduler {
     
+    //the definition of a strech of time
+    private class Strech{
+        private int startSecond;
+        private int endSecond;
+    }
     
-    private class player{
-        private double[] timeStretches;
+    //the definition of a player with what this class needs to know.
+    private class Player{
+        private Strech[] timeStretches;
         private String name;
         
-        public player(String name, int streches){
-            this.timeStretches = new double[streches];
-            this.name = name;
+        public Player(String name, int streches){
+            this.timeStretches = new Strech[streches];   //make sure array has apporpirate amount of space
+            this.name = name;                            //update this players name
         }
     }
     
-    private player playerOne;
+    private Player playerOne;
     
-    public Scheduler(String name, int streches, double[] arr){
-        this.playerOne = new player(name,streches);
-        playerOne.timeStretches = arr;
+    public Scheduler(String name, int streches, Strech[] arr){
+        this.playerOne = new Player(name,streches);            //make the first player
+        playerOne.timeStretches = arr;                         //load in the timestreches
     }
     
     

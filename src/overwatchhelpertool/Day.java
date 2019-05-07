4c55c09a5f308efe2b7cebb06d5a5e3ed83ca9aa
Phillip <Phillip@192.168.110.22>
2019-05-07 19:46:10 +0200

    renamed timeStrech class and added methods to Day to add streches of time.

--- src/overwatchhelpertool/Day.java
+++ src/overwatchhelpertool/Day.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * the base structure of a day in a schedule
  */
 package overwatchhelpertool;
 import java.util.ArrayList;
@@ -11,7 +9,17 @@ import java.util.ArrayList;
  */
 public class Day {
     public String name;
-    private ArrayList timeStreches = new ArrayList();
+    private ArrayList timeStreches = new ArrayList();  //an array of streches
     
+    public Day(){
+        this.name = "Monday";  
+    }
     
+    public Day(String name){
+        this.name = name;
+    }
+    
+    public void addStrech(TimeStrech strech){
+        timeStreches.add(strech);
+    }
 }
---
+++ src/overwatchhelpertool/TimeStrech.java
@@ -0,0 +1,31 @@
+/*
+ * the base strucure of a time strech in schedule
+ */
+package overwatchhelpertool;
+
+/**
+ *
+ * @author Phillip
+ */
+public class TimeStrech {
+    
+        //the definition of a strech of time
+        private int startSecond;
+        private int endSecond;
+        
+        public TimeStrech(int start, int end){
+            this.startSecond = start;
+            this.endSecond = end;
+        }
+        
+        //gives the start second if needed
+        public int giveStart(){
+            return startSecond;
+        }
+        //gives the end second if needed 
+        public int giveEnd(){
+            return endSecond;
+        }
+    
+    
+}

0300ffdb17de68faa65c312ab5771dfa9e9ed0f7
Phillip <Phillip@192.168.110.22>
2019-05-08 17:18:01 +0200

    add test but it is not working because of framework error

---
+++ test/overwatchhelpertool/PlayerSchedulerTest.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package overwatchhelpertool;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author Phillip
+ */
+public class PlayerSchedulerTest {
+    
+    public PlayerSchedulerTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    /**
+     * Test of buildSchedule method, of class PlayerScheduler.
+     */
+    @Test
+    public void testBuildSchedule() {
+        System.out.println("buildSchedule");
+        PlayerScheduler instance = null;
+        Schedule expResult = null;
+        Schedule result = instance.buildSchedule();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}

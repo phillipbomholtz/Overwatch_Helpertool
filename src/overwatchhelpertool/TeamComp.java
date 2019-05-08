cb6958ba68003b803f592313ae9726f97d4dd501
Phillip <Phillip@192.168.110.22>
2019-05-08 19:48:07 +0200

    restructured teamcomp

--- src/overwatchhelpertool/TeamComp.java
+++ src/overwatchhelpertool/TeamComp.java
@@ -10,12 +10,22 @@ import java.util.ArrayList;
  * @author Phillip Bomholtz
  */
 public class TeamComp {
-   private String name;
-   private String note;
+   private String name;          //the name of the comp
+   private String note;          //the entered note
    private List<Map> maps;       //a list of maps of map struct
-   private String Heros;         //a string with all heros in comma separeted style
+   private String Hero1;         //name of hero
+   private String Hero2;         //name of hero
+   private String Hero3;         //name of hero
+   private String Hero4;         //name of hero
+   private String Hero5;         //name of hero
+   private String Hero6;         //name of hero
+
    
    public TeamComp(){
        
+   }
+   
+   public TeamComp(String compName,String Note){
+       name = compName;
    }
 } 

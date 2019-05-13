ac258b64eb9b9653cb61a4370e7f6b38f6e2a50f
Phillip <Phillip@192.168.110.22>
2019-05-13 09:59:18 +0200

    revambing player scedule by extracting panels to make shorter

--- src/overwatchhelpertool/Menu.java
+++ src/overwatchhelpertool/Menu.java
@@ -33,17 +33,19 @@ public class Menu extends javax.swing.JPanel {
         jTabbedPane1 = new javax.swing.JTabbedPane();
         playerLogin1 = new overwatchhelpertool.PlayerLogin();
         playerMenu1 = new overwatchhelpertool.PlayerMenu();
+        playerScheduleMenu1 = new overwatchhelpertool.PlayerScheduleMenu();
 
         setPreferredSize(new java.awt.Dimension(1000, 500));
 
         jTabbedPane1.addTab("tab2", playerLogin1);
         jTabbedPane1.addTab("tab1", playerMenu1);
+        jTabbedPane1.addTab("tab3", playerScheduleMenu1);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jTabbedPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 1007, Short.MAX_VALUE)
+            .addComponent(jTabbedPane1)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -68,5 +70,6 @@ public class Menu extends javax.swing.JPanel {
     private javax.swing.JTabbedPane jTabbedPane1;
     private overwatchhelpertool.PlayerLogin playerLogin1;
     private overwatchhelpertool.PlayerMenu playerMenu1;
+    private overwatchhelpertool.PlayerScheduleMenu playerScheduleMenu1;
     // End of variables declaration//GEN-END:variables
 }
